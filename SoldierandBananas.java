070de1167b6b816ac5a46029a525cb69b39da78d
Bithika-Jain <bithika.jainn@gmail.com>
2026-07-28 20:05:04 +0530

    Solved 546A - Soldier and Bananas on 28th July

---
+++ SoldierandBananas.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class SoldierandBananas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int totalCost = k * w * (w + 1) / 2;
+        int borrow = totalCost - n;
+
+        System.out.println(Math.max(0, borrow));
+
+        sc.close();
+    }
+}

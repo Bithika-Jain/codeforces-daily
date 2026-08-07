8fc52826dc578a92237f6622ec59c45e1cdd4415
Bithika-Jain <bithika.jainn@gmail.com>
2026-08-08 01:56:00 +0530

    Solved 1030A - In Search of an Easy Problem on 8th August

---
+++ InSearchofanEasyProblem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class InSearchofanEasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        boolean hard = false;
+
+        for (int i = 0; i < n; i++) {
+            int response = sc.nextInt();
+
+            if (response == 1) {
+                hard = true;
+            }
+        }
+
+        if (hard) {
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
+
+        sc.close();
+    }
+}
